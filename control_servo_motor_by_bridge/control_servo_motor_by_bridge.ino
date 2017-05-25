6cc42e550aa6b0b811df09450ebf8b78bc1ccdc8
Asraf Uddin Ahmed <13ratul@gmail.com>
2017-05-25 10:32:33 +0600

    Servo motor can handle left and right request

--- control_servo_motor_by_bridge/control_servo_motor_by_bridge.ino
+++ control_servo_motor_by_bridge/control_servo_motor_by_bridge.ino
@@ -10,6 +10,12 @@
 #include <Servo.h>
 #include <Console.h>
 #include <Bridge.h>
+#include <BridgeServer.h>
+#include <BridgeClient.h>
+
+// Listen to the default port 5555, the Yún webserver
+// will forward there all the HTTP requests you send
+BridgeServer server;
 
 Servo myservo;  // create servo object to control a servo
 // twelve servo objects can be created on most boards
@@ -20,14 +26,44 @@ void setup() {
   Bridge.begin();
   Console.begin();
   myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+
+  // Listen for incoming connection only from localhost
+  // (no one from the external network could connect)
+  server.listenOnLocalhost();
+  server.begin();
 }
 
 void loop() {
+  BridgeClient client = server.accept();
+  if (client) {
+    String command = client.readStringUntil('/');
+    if (command == "digital") {
+      digitalCommand(client);
+    }
+    else if (command == "servo") {
+      servoCommand(client);
+    }
+    // Close connection and free resources.
+    client.stop();
+  }
+  
   if (Console.available() > 0) {
     pos = Console.parseInt();
     Console.parseInt();
     Console.println(pos);
     myservo.write(pos);              // tell servo to go to position in variable 'pos'
   }
+}
+
+void servoCommand(BridgeClient client) {
+  int value, currentPos;
+  value = client.parseInt();
+  currentPos = value + pos;
+  pos = currentPos > 0 && currentPos <= 180 ? currentPos : pos;
+  myservo.write(pos);
+}
+
+void digitalCommand(BridgeClient client) {
+  Console.println("digital command");
 }
 
